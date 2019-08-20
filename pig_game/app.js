1b831952d9fcfa4262b439d6ea4eabd18d9c834f
youquanliu <youquan.liu@hotmail.com>
2019-08-19 21:17:41 -0400

    Add user input feasure

--- pig_game/app.js
+++ pig_game/app.js
@@ -20,88 +20,100 @@ init();
 
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
-    
-    if(gamePlaying){
-    
-    //1.Random number
-    var dice = Math.floor(Math.random() * 6) + 1;
-    
-    //2.Display the result
-    var diceDOM = document.querySelector('.dice');
-    
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
 
-    //3.Update the roud score IF the roll number is not ONE
-    if(dice !== 1){
-        //add score
-        roundScore += dice    
-        document.querySelector('#current-' + activePlayer).textContent = roundScore;
-        
-    }else{
-        //next player
-        nextPlayer();
+    if(gamePlaying){
+
+        //1.Random number
+        var dice = Math.floor(Math.random() * 6) + 1;
+
+        //2.Display the result
+        var diceDOM = document.querySelector('.dice');
+
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+
+        //3.Update the roud score IF the roll number is not ONE
+        if(dice !== 1){
+
+            //add score
+            roundScore += dice    
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+        }else{
+            //next player
+            nextPlayer();
         }
+
     }
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function(){
-    
+
     if(gamePlaying){
+        
         //add new hold score to exist score
-    
         scores[activePlayer] += roundScore;
-             
+
         //transfer current score to hold score, 0 the current
         document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
         document.getElementById('current-' + activePlayer).textContent = 0;
+        
+        var input = document.querySelector('.final-score').value;
+        var winningScore;
+        
+        //Underfined , 0, null, or "" are coerced to false
+        if(input){
+            winningScore = input;
+        }else{
+            winningScore = 100;
+        }
     
         //the player win if score is more than 100
-        if(scores[activePlayer] >=100){
+        if(scores[activePlayer] >= winningScore){
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
             gamePlaying = false;
         }else{
-        //next player
+            //next player
             nextPlayer();
         }
     }
-    
+
 })
 //passing init insead of calling as init()
 document.querySelector('.btn-new').addEventListener('click',init);
 
 function nextPlayer(){
-    
+
     //next player
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
-    
+
     document.getElementById('current-0').textContent = '0'
     document.getElementById('current-1').textContent = '0'
-    
+
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
-        
+
     document.querySelector('.dice').style.display = 'none';
 }
 
 function init(){
-    
+
     scores = [0,0];
     activePlayer = 0;
     roundScore = 0;
     gamePlaying = true;
-    
+
     document.querySelector('.dice').style.display = 'none';
 
     document.getElementById('score-0').textContent = '0'
     document.getElementById('score-1').textContent = '0'
     document.getElementById('current-0').textContent = '0'
     document.getElementById('current-1').textContent = '0'
-    
+
     document.getElementById('name-0').textContent = 'Player 1';
     document.getElementById('name-1').textContent = 'Player 2';
     document.querySelector('.player-0-panel').classList.remove('winner');
